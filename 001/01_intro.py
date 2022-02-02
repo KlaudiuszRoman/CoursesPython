01c9a5868c3090dbf530fa62fe54eea1b40aa04a
KlaudiuszRoman <klaudiuszr@gmail.com>
2022-02-02 12:43:36 +0100

    Update 01_intro.py
    Python - more advenced usage of print function

--- 001/01_intro.py
+++ 001/01_intro.py
@@ -58,8 +58,66 @@ print('Python\n3.9')
 print("""Python
 3.7""")
 
+# %%
+a = 10
 
+# %%
+print('\tPython')
+print('\t\t\tPython')
 
+# %%
+print('C:\path\\to\something\\new')
+
+# %%
+print(r'C:\path\to\something\new')
+
+# %%
+print('C:\\path\\to\\something\\new')
+
+# %%
+import os
+os.getcwd()
+
+# %%
+print("""
+Instrukcja uruchamiania pliku przyklad.py:
+
+    --file [nazwa pliku]
+        zapisuje output do pliku
+        
+    --qiuet
+       wycisza logi w konsoli 
+Koniec.""")
+
+# %%
+text = 'I love Python. '
+print(text * 3)
+print('how...' * 10)
+print('-' * 30)
+
+# %%
+'Python'
+'Py' 'thon'
+print('Py' 'thon')
+
+# %%
+url = 'https://test.pl/longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong.png'
+
+url2 = ('https://test.pl/longlonglonglonglonglonglonglonglonglonglonglong'
+        'longlonglonglonglonglong.png')
+
+# %%
+name = 'Python'
+print(name + ' 3.9')
+print(name, ' 3.9')
+
+# %%
+age = 27
+imie = 'Klaudiusz'
+
+print(imie + ' ' +str(age))
+print(imie,age)
+print('{0} ma {1} lat.'.format(imie, age))
 
 
 
