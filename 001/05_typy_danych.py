737415a8d14dd2c03c7659528ac2a51b2c2011d5
Klaudiusz Roman <klaudiuszr@gmail.com>
2022-02-07 11:52:02 +0100

    Data types in Python

---
+++ 001/05_typy_danych.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+string = 'Python'
+
+print(dir(string)) # wszytkie atrybuty i metody string
+
+# %%
+a = 10
+print(dir(a))
+
+# %%
+print(type(a))
+print(type(string))
+
+# %%
+b = 4.5
+print(type(b))
+
+# %%
+d = 3 + 3j
+print(type(d))
+
+# %%
+print(type(True))
+
+value = True
+print(value)
+
+# %% Zadanie 4
+
+x = '1323435'
+y = 12334
+z = '0'
+
+print('x: {}\ny: {}\nz: {}'.format(type(x), type(y), type(z)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
