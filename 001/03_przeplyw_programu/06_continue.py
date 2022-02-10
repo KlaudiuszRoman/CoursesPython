9b450d5ce4414d34310e0d715b47718bdde45c64
Klaudiusz Roman <klaudiuszr@gmail.com>
2022-02-10 11:06:28 +0100

    Continue instruction in Python

---
+++ 001/03_przeplyw_programu/06_continue.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# %%
+for i in range(10):
+    if i == 6:
+        continue
+    print(i)
+    
+# %%
+for i in range(20):
+    if i % 2 == 0:
+        continue
+    print(i)
+    
+# %%
+for i in range(20):
+    if i % 2 == 1:
+        continue
+    print(i)
+
+# %%
+sample = 'Python Course'
+for char in sample:
+    if char == ' ':
+        continue
+    print(char)
+    
+# %%
+hashtags = '#summer#holiday#free'
+result = ''
+for char in hashtags:
+    if char == '#':
+        print(result)
+        result = ''
+        continue
+    result += char
+print(result)
+
+# %% Ćwiczenie 9
+lista = [1, 2, 99, 4, 5]
+for number in lista:
+    if number == 99:
+        continue
+    print(number)
