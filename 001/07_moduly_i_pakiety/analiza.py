7f8f4d4651d6f1967d2ffb46c3dd3e720b021632
Klaudiusz Roman <klaudiuszr@gmail.com>
2022-02-15 12:40:57 +0100

    Modules and packages in Python

---
+++ 001/07_moduly_i_pakiety/analiza.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import rocket_science
+
+# %%
+dir(rocket_science)
+
+# %%
+rocket_science.calculate_mean([3, 4])
+rocket_science.calculate_min([1, 2, 3])
+rocket_science.calculate_max([1, 2, 3])
+
+# %%
+
+from rocket_science import calculate_mean
+
+# %%
+calculate_mean([3, 4])
+
+# %%
+from rocket_science import *
+
+# %%
+calculate_max([5, 7])
+
+# %%
+from rocket_science import calculate_min, calculate_max
+
+# %%
+calculate_min([2, 1])
+
+# %%
