d2095a8803d20a2f21173057260e8432a66d0b16
Klaudiusz Roman <klaudiuszr@gmail.com>
2022-02-15 15:02:15 +0100

    Modules and packages build in Python

---
+++ 001/08_wbudowane_pakiety/hello.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+print('Hello world!')
+
+for i in range(10):
+    print('*' * i)
+    
+# %%
+if __name__ == '__main__':
+    pass
---
+++ 001/08_wbudowane_pakiety/test/first.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+print('Nazwa modułu: ', __name__)
+
+if __name__ == '__main__':
+    print('Uruchamiamy bezpośrednio')
+else:
+    print('Uruchamiamy z importu')
